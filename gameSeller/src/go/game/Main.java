d18e87cb23341618409b55584b29edd097909cb6
wns3915 <wns3915@naver.com>
2024-07-15 17:52:48 +0900

    프로젝트 옮기기 완료

---
+++ gameSeller/src/go/game/Main.java
@@ -0,0 +1,13 @@
+package go.game;
+
+import go.game.control.GamesellerControl;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// 프로그램의 시작
+		GamesellerControl gamesellerControl = new GamesellerControl();
+		gamesellerControl.run();
+	}
+
+}
